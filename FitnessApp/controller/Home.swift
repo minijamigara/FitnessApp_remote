255cd1ada15a9b167dfd9b6beefcf0381a8071cf
minijamigara <mahdse201f002@student.nibm.lk>
2023-05-21 01:37:08 +0530

    profile view controller designed

--- FitnessApp/controller/Home.swift
+++ FitnessApp/controller/Home.swift
@@ -42,17 +42,329 @@ class NotifyVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Notifications"
 
         // Do any additional setup after loading the view.
     }
 
 }
 class AccountVC: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let userLbl = UILabel();
+    let joinTimeLbl = UILabel();
+    let dateLbl = UILabel();
+    let imageButton = UIButton()
+    
+    let rectrangleTotHr = UIView()
+    let totLbl = UILabel();
+    let totValLbl = UILabel();
+    
+    let rectrangleBMI = UIView()
+    let BMILbl = UILabel();
+    let BMIValLbl = UILabel();
+    
+    let usernameTxt = UITextField()
+    let saveBtn = UIButton();
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .black
+        title = "Profile"
+        
+        let imageName = "user.png"
+        let image = UIImage(named: imageName)
+        let imageView = UIImageView(image: image!)
+        
+        //imageView.contentMode = .scaleAspectFit
+        imageView.frame = CGRect(x: 120, y: 180, width: 160, height: 160)
+        imageView.layer.masksToBounds = false
+        imageView.layer.borderColor = UIColor.gray.cgColor
+        imageView.layer.borderWidth = 2
+        imageView.layer.cornerRadius = imageView.frame.size.height/2
+        imageView.clipsToBounds = true
+        view.addSubview(imageView)
+        
+        configureLoginLableText()
+        configureImgBtn()
+        configureJoinTimeLbl()
+        configureDateLbl()
+        
+        setupTotHrContainer()
+        setupBMIContainer()
+        
+        configureUsernameTxt()
+        configureSaveBtn()
+        
+        let db = Firestore.firestore()
+        let currentUser = Auth.auth().currentUser
+        let email = currentUser?.email
+        let collectionRef = db.collection("user_tbl")
+        let docRef = collectionRef.document(email!)
+        db.collection("user_tbl").document(email!).getDocument { [self] (document, error) in
+            if let document = document, document.exists {
+                let data = document.data()
+                
+                if let bmi = data?["BMI"] as? Int {
+                    print("BMI: \(bmi)")
+                    BMIValLbl.text = String(bmi)
+                }
+                if let name = data?["username"] as? String {
+                    print("Name: \(name)")
+                    userLbl.text = String(name)
+                }
 
-        // Do any additional setup after loading the view.
+            } else {
+                print("Document does not exist or there was an error: \(error?.localizedDescription ?? "Unknown error")")
+            }
+        }
+        
+    }
+    func configureLoginLableText(){
+        userLbl.text = "Minija"
+        userLbl.textAlignment = .center
+        userLbl.textColor = .white
+        userLbl.font = UIFont(name: userLbl.font.fontName, size: 22)
+        userLbl.translatesAutoresizingMaskIntoConstraints = false
+        
+        setUpUserLable()
+    }
+    func setUpUserLable(){
+        view.addSubview(userLbl)
+        
+        NSLayoutConstraint.activate([
+            
+            userLbl.widthAnchor.constraint(equalToConstant: 350),
+            userLbl.heightAnchor.constraint(equalToConstant: 40),
+            userLbl.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            userLbl.bottomAnchor.constraint(equalTo: view.topAnchor,constant: 380)
+            ])
+    }
+    
+    func configureImgBtn(){
+        imageButton.setImage(UIImage(named: "edit.png"), for: .normal)
+        imageButton.imageView?.contentMode = .scaleAspectFit
+        imageButton.addTarget(self, action: #selector(imageButtonTapped), for: .touchUpInside)
+        view.addSubview(imageButton)
+        
+        setUpImgBtn()
+    }
+    func setUpImgBtn(){
+        
+        imageButton.layer.cornerRadius = 20
+        
+        imageButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            imageButton.widthAnchor.constraint(equalToConstant: 45),
+            imageButton.heightAnchor.constraint(equalToConstant: 45),
+            imageButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        imageButton.addTarget(self, action: #selector(imageButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc func imageButtonTapped() {
+        // Implement your button tap action here
+    }
+    
+    func configureJoinTimeLbl(){
+        joinTimeLbl.text = "Joined"
+        joinTimeLbl.textAlignment = .left
+        joinTimeLbl.textColor = .gray
+        joinTimeLbl.translatesAutoresizingMaskIntoConstraints = false
+        
+        setUpJoinTimeLbl()
+    }
+    func setUpJoinTimeLbl(){
+        view.addSubview(joinTimeLbl)
+        
+        NSLayoutConstraint.activate([
+            
+            joinTimeLbl.widthAnchor.constraint(equalToConstant: 260),
+            joinTimeLbl.heightAnchor.constraint(equalToConstant: 40),
+            //joinTimeLbl.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            joinTimeLbl.bottomAnchor.constraint(equalTo: view.topAnchor,constant: 410)
+            ])
+        joinTimeLbl.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 125).isActive = true
+    }
+    func configureDateLbl(){
+        dateLbl.text = "May 2023"
+        dateLbl.textAlignment = .right
+        dateLbl.textColor = .gray
+        dateLbl.translatesAutoresizingMaskIntoConstraints = false
+        
+        setUpDateLbl()
+    }
+    func setUpDateLbl(){
+        view.addSubview(dateLbl)
+        
+        NSLayoutConstraint.activate([
+            
+            dateLbl.widthAnchor.constraint(equalToConstant: 260),
+            dateLbl.heightAnchor.constraint(equalToConstant: 40),
+            dateLbl.bottomAnchor.constraint(equalTo: view.topAnchor,constant: 410)
+            ])
+        joinTimeLbl.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 50).isActive = true
+    }
+    
+    //tot hour container
+    func setupTotHrContainer(){
+        rectrangleTotHr.frame = CGRect(x:30, y: 420, width: 150 , height: 70)
+        rectrangleTotHr.backgroundColor = UIColor.orange
+        rectrangleTotHr.layer.opacity = 0.4
+        rectrangleTotHr.layer.cornerRadius = 20
+        self.view.addSubview(rectrangleTotHr)
+        
+        totValLbl.text = "102"
+        totValLbl.textAlignment = .center
+        totValLbl.textColor = .white
+        totValLbl.font = UIFont(name: totValLbl.font.fontName, size: 24)
+        totValLbl.translatesAutoresizingMaskIntoConstraints = false
+        rectrangleTotHr.addSubview(totValLbl)
+        NSLayoutConstraint.activate([
+            
+            totValLbl.widthAnchor.constraint(equalToConstant: 350),
+            totValLbl.heightAnchor.constraint(equalToConstant: 30),
+            totValLbl.centerXAnchor.constraint(equalTo: rectrangleTotHr.centerXAnchor),
+            //totLbl.centerYAnchor.constraint(equalTo: rectrangleTotHr.centerYAnchor),
+            ])
+        totValLbl.topAnchor.constraint(equalTo: rectrangleTotHr.topAnchor, constant: 5).isActive = true
+        
+        totLbl.text = "Total hours"
+        totLbl.textAlignment = .center
+        totLbl.textColor = .white
+        totLbl.translatesAutoresizingMaskIntoConstraints = false
+        rectrangleTotHr.addSubview(totLbl)
+        NSLayoutConstraint.activate([
+            
+            totLbl.widthAnchor.constraint(equalToConstant: 350),
+            totLbl.heightAnchor.constraint(equalToConstant: 40),
+            totLbl.centerXAnchor.constraint(equalTo: rectrangleTotHr.centerXAnchor),
+            //totLbl.centerYAnchor.constraint(equalTo: rectrangleTotHr.centerYAnchor),
+            ])
+        totLbl.topAnchor.constraint(equalTo: rectrangleTotHr.topAnchor, constant: 30).isActive = true
+    }
+    
+    //BMI container
+    func setupBMIContainer(){
+        rectrangleBMI.frame = CGRect(x:210, y: 420, width: 150 , height: 70)
+        rectrangleBMI.backgroundColor = UIColor.orange
+        rectrangleBMI.layer.opacity = 0.4
+        rectrangleBMI.layer.cornerRadius = 20
+        self.view.addSubview(rectrangleBMI)
+        
+        BMIValLbl.text = ""
+        BMIValLbl.textAlignment = .center
+        BMIValLbl.textColor = .white
+        BMIValLbl.font = UIFont(name: BMIValLbl.font.fontName, size: 24)
+        BMIValLbl.translatesAutoresizingMaskIntoConstraints = false
+        rectrangleBMI.addSubview(BMIValLbl)
+        NSLayoutConstraint.activate([
+            
+            BMIValLbl.widthAnchor.constraint(equalToConstant: 350),
+            BMIValLbl.heightAnchor.constraint(equalToConstant: 30),
+            BMIValLbl.centerXAnchor.constraint(equalTo: rectrangleBMI.centerXAnchor),
+            //totLbl.centerYAnchor.constraint(equalTo: rectrangleTotHr.centerYAnchor),
+            ])
+        BMIValLbl.topAnchor.constraint(equalTo: rectrangleBMI.topAnchor, constant: 5).isActive = true
+        
+        BMILbl.text = "BMI"
+        BMILbl.textAlignment = .center
+        BMILbl.textColor = .white
+        BMILbl.translatesAutoresizingMaskIntoConstraints = false
+        rectrangleBMI.addSubview(BMILbl)
+        NSLayoutConstraint.activate([
+            
+            BMILbl.widthAnchor.constraint(equalToConstant: 350),
+            BMILbl.heightAnchor.constraint(equalToConstant: 40),
+            BMILbl.centerXAnchor.constraint(equalTo: rectrangleBMI.centerXAnchor),
+            //totLbl.centerYAnchor.constraint(equalTo: rectrangleTotHr.centerYAnchor),
+            ])
+        BMILbl.topAnchor.constraint(equalTo: rectrangleBMI.topAnchor, constant: 30).isActive = true
+    }
+    
+    //username
+    func configureUsernameTxt(){
+        usernameTxt.returnKeyType = .done
+        usernameTxt.autocorrectionType = .no
+        usernameTxt.layer.borderWidth = 1.5
+        usernameTxt.layer.borderColor = UIColor.white.cgColor
+        usernameTxt.placeholder = "Username"
+        usernameTxt.text = ""
+        usernameTxt.autocapitalizationType = .none
+        usernameTxt.textColor = .gray
+        usernameTxt.textAlignment = .center
+
+        
+        SetupUsernameTxt()
+    }
+    func SetupUsernameTxt(){
+        self.view.addSubview(usernameTxt)
+        
+        usernameTxt.layer.cornerRadius = 20
+        usernameTxt.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            
+            usernameTxt.widthAnchor.constraint(equalToConstant: 320),
+            usernameTxt.heightAnchor.constraint(equalToConstant: 50),
+            usernameTxt.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        ])
+        usernameTxt.topAnchor.constraint(equalTo: view.topAnchor, constant: 550).isActive = true
+    }
+    
+    //save button
+    func configureSaveBtn(){
+        saveBtn.configuration = .gray()
+        saveBtn.configuration?.baseForegroundColor = .black
+        saveBtn.configuration?.cornerStyle = .medium
+        saveBtn.layer.borderWidth = 1
+        saveBtn.layer.borderColor = UIColor.orange.cgColor
+        saveBtn.layer.backgroundColor = UIColor.orange.cgColor
+
+        saveBtn.setTitle("Save", for: .normal)
+        //loginBtn.setImage(UIImage(systemName: "arrow.forward"), for: .normal)
+        saveBtn.semanticContentAttribute = .forceRightToLeft
+        
+        setupSaveBtn()
+    }
+    
+    func setupSaveBtn(){
+        self.view.addSubview(saveBtn)
+        
+        saveBtn.layer.cornerRadius = 20
+        
+        saveBtn.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            saveBtn.widthAnchor.constraint(equalToConstant: 320),
+            saveBtn.heightAnchor.constraint(equalToConstant: 50),
+            saveBtn.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            //loginBtn.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        saveBtn.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -111).isActive = true
+        /*loginBtn.rightAnchor.constraint(equalTo: view.rightAnchor,constant: -28).isActive = true*/
+        
+        
+        saveBtn.addTarget(self, action: #selector(saveProfile), for: .touchUpInside)
+    }
+    
+    @objc func saveProfile(){
+        let db = Firestore.firestore()
+        let currentUser = Auth.auth().currentUser
+        let email = currentUser?.email
+        let collectionRef = db.collection("user_tbl")
+        let docRef = collectionRef.document(email!)
+        let user = usernameTxt.text
+        docRef.updateData(["username": user as Any]) { error in
+            if let error = error {
+                // Handle the error
+                print("Error updating document: \(error)")
+            } else {
+                // Field added successfully
+                print("Field successfully added")
+            }
+        }
     }
 
 }
@@ -76,6 +388,7 @@ class Landing: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Home"
         
         
         // Fetch the data from Firestore
